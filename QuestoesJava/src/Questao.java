692c3ae96e677cb1e1e7eb95c0631205dee177dd
Gustavo Vitor <gustavovitorau@gmail.com>
2024-11-17 01:17:24 -0300

    Start of the construction of the "Questao" class in the program.

---
+++ QuestoesJava/src/Questao.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Questao {
+    String pergunta = "";
+    String opcaoA = "";
+    String opcaoB = "";
+    String opcaoC = "";
+    String opcaoD = "";
+    String opcaoE = "";
+    String correta = "";
+
+    public boolean isCorreta(String resposta) {
+        if (resposta.equalsIgnoreCase(this.correta)) {
+            System.out.println("Parabéns, resposta Correta! - Letra: " + this.correta);
+            System.out.println("");
+            return true;
+        } else {
+            System.out.println("Resposta Errada!");
+            System.out.println("A opção correta é a letra: " + this.correta);
+            System.out.println("");
+            return false;
+        }
+    }
