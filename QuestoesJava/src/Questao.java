03c84f2065f78108d7d408087b3b60a761e200d9
Gustavo Vitor <gustavovitorau@gmail.com>
2024-11-17 22:52:51 -0300

    Added method for reading and validating multiple-choice answers.

--- QuestoesJava/src/Questao.java
+++ QuestoesJava/src/Questao.java
@@ -21,3 +21,23 @@ public class Questao {
             return false;
         }
     }
+
+    public String leiaResposta() {
+        Scanner ler = new Scanner(System.in);
+        String resp;
+        do {
+            System.out.print("Digite a resposta: ");
+            resp = ler.next();
+        } while (!respostaValida(resp));
+        return resp;
+    }
+
+    private boolean respostaValida(String resp) {
+        if (resp.equalsIgnoreCase("A") || resp.equalsIgnoreCase("B") || resp.equalsIgnoreCase("C") ||
+                resp.equalsIgnoreCase("D") || resp.equalsIgnoreCase("E")) {
+            return true;
+        }
+        System.out.println("Resposta inválida! Digite opção A, B, C, D ou E.");
+        System.out.println("");
+        return false;
+    }
