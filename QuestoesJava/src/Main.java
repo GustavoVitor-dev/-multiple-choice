7365bbab3acba12e38418a883a366980170134ef
Gustavo Vitor <gustavovitorau@gmail.com>
2024-11-24 09:46:25 -0300

    Solving the redundancy of class duplication - The display of the question number that was requested is already implemented by a loop that will show the number of each question on the screen.

--- QuestoesJava/src/Main.java
+++ QuestoesJava/src/Main.java
@@ -1,65 +1,6 @@
 import java.util.Scanner;
 
 public class Main {
-    // Classe interna que representa uma questão do quiz
-    static class Questao {
-        // Atributos para armazenar a pergunta, opções de resposta e a resposta correta
-        String pergunta = "";
-        String opcaoA = "";
-        String opcaoB = "";
-        String opcaoC = "";
-        String opcaoD = "";
-        String opcaoE = "";
-        String correta = "";
-
-        // Verifica se a resposta do usuário está correta
-        public boolean isCorreta(String resposta) {
-            if (resposta.equalsIgnoreCase(this.correta)) {
-                System.out.println("Parabéns, resposta Correta! - Letra: " + this.correta);
-                System.out.println("");
-                return true;
-            } else {
-                System.out.println("Resposta Errada!");
-                System.out.println("A opção correta é a letra: " + this.correta);
-                System.out.println("");
-                return false;
-            }
-        }
-
-        // Lê a resposta do usuário e valida a entrada
-        public String leiaResposta() {
-            Scanner ler = new Scanner(System.in);
-            String resp;
-            do {
-                System.out.print("Digite a resposta: ");
-                resp = ler.next(); // Captura a resposta do usuário
-            } while (!respostaValida(resp)); // Garante que a resposta é válida (A, B, C, D ou E)
-            return resp;
-        }
-
-        // Valida se a resposta do usuário está dentro das opções permitidas
-        private boolean respostaValida(String resp) {
-            if (resp.equalsIgnoreCase("A") || resp.equalsIgnoreCase("B") || resp.equalsIgnoreCase("C") ||
-                    resp.equalsIgnoreCase("D") || resp.equalsIgnoreCase("E")) {
-                return true;
-            }
-            System.out.println("Resposta inválida! Digite opção A, B, C, D ou E.");
-            System.out.println("");
-            return false;
-        }
-
-        // Exibe a pergunta e as opções de resposta
-        public void escrevaQuestao() {
-            System.out.println(this.pergunta);
-            System.out.println(this.opcaoA);
-            System.out.println(this.opcaoB);
-            System.out.println(this.opcaoC);
-            System.out.println(this.opcaoD);
-            System.out.println(this.opcaoE);
-            System.out.println();
-        }
-    }
-
     public static void main(String[] args) {
     System.out.println("UNIFAN - Centro Universitário Alfredo Nasser");
     System.out.println("Aluno: Gustavo Augusto Oliveira Vitor");
